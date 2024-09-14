0e35fc70be4043d9a699d0350af3166baf1a55b8
Miguel Rodrigues Carrilho Borges <up202004481@edu.fe.up.pt>
2024-09-14 16:22:30 +0200

    'enter' key function

--- tools/smenu/src/main.c
+++ tools/smenu/src/main.c
@@ -1,6 +1,7 @@
 #include "c.h"
 #include <windows.h>
 #include <Shlobj.h>
+#include <shlwapi.h>
 #include <stdio.h>
 #include <io.h>
 
@@ -12,22 +13,27 @@ enum {
 
 static HBRUSH background_brush;
 static HBRUSH background_brush2;
+static HBRUSH foreground_brush;
 static u16 input[INPUT_BUF_SIZE];
 static u8 input_len;
-static HFONT ui_font;
+static HFONT search_font;
+static HFONT options_font;
+static u8 selected;
 
 static u16 widestr_alloc[WIDESTRING_ALLOC_BUF_SIZE];
 static u16 widestr_alloc_pos = 0;
 static u16 widestrs[WIDESTRING_MAX_STRINGS];
 static u16 widestrs_count = 0;
-static u16 last_local_user_application;
+static u16 matching_strs[WIDESTRING_MAX_STRINGS];
+static u16 matching_count = 0;
+static u16 last_user_application;
 static unsigned char run_as_app_launcher;
 
 static LRESULT CALLBACK smenuProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 static u32 isNotInvalidAscii(u16 codepoint);
 
 static void parseStdin(void);
-static void parseFolder(u16 *path);
+static void parseFolder(u16 *path, u8 apps_root_len);
 static int sortStrings(const void *sip1, const void *sip2);
 
 int main(void) {
@@ -56,21 +62,21 @@ int main(void) {
 	run_as_app_launcher = _isatty(_fileno(stdin)) ? 1u : 0u;
 	if (run_as_app_launcher) {
 		u16 tmp_buff[MAX_PATH];
-		SHGetFolderPathW(NULL, CSIDL_STARTMENU, NULL, SHGFP_TYPE_CURRENT, tmp_buff);
-		wcscat(tmp_buff, L"\\Programs\\*");
-		parseFolder(tmp_buff);
-		last_local_user_application = widestrs_count;
-		SHGetFolderPathW(NULL, CSIDL_COMMON_STARTMENU, NULL, SHGFP_TYPE_CURRENT, tmp_buff);
-		wcscat(tmp_buff, L"\\Programs\\*");
-		parseFolder(tmp_buff);
+		SHGetFolderPathW(NULL, CSIDL_PROGRAMS, NULL, SHGFP_TYPE_CURRENT, tmp_buff);
+		size_t len = wcslen(tmp_buff);
+		memcpy(tmp_buff + len, L"\\*", sizeof(L"\\*"));
+		parseFolder(tmp_buff, len+1);
+		last_user_application = widestr_alloc_pos;
+		SHGetFolderPathW(NULL, CSIDL_COMMON_PROGRAMS, NULL, SHGFP_TYPE_CURRENT, tmp_buff);
+		len = wcslen(tmp_buff);
+		memcpy(tmp_buff + len, L"\\*", sizeof(L"\\*"));
+		parseFolder(tmp_buff, len+1);
 	} else {
 		parseStdin();
 	}
 	qsort(widestrs, widestrs_count, sizeof(widestrs[0]), sortStrings);
-
-	for (size_t i = 0; i < widestrs_count; i += 1) {
-		wprintf(L"%s\n", widestr_alloc + widestrs[i]);
-	}
+	memcpy(matching_strs, widestrs, widestrs_count * sizeof(widestrs[0]));
+	matching_count = widestrs_count;
 
 	POINT cursor_pos;
 	GetCursorPos(&cursor_pos);
@@ -88,8 +94,10 @@ int main(void) {
 	int win_top = (win_height >> 1) + current_mon_info.rcMonitor.top;
 	background_brush = CreateSolidBrush(DEFAULT_BACKGROUND_COLOR);
 	background_brush2 = CreateSolidBrush(DEFAULT_BACKGROUND_COLOR2);
+	foreground_brush  = CreateSolidBrush(DEFAULT_FOREGROUND_COLOR);
 
-	ui_font = CreateFontW(DEFAULT_FONT_SIZE, 0, 0, 0, FW_NORMAL, 0, 0, 0, 0, 0, 0, 0, 0, L"Tahoma");
+	search_font = CreateFontW(DEFAULT_SEARCH_FONT_SIZE, 0, 0, 0, FW_NORMAL, 0, 0, 0, 0, 0, 0, 0, 0, L"Tahoma");
+	options_font = CreateFontW(DEFAULT_OPTIONS_FONT_SIZE, 0, 0, 0, FW_NORMAL, 0, 0, 0, 0, 0, 0, 0, 0, L"Tahoma");
 
 	HWND parent = CreateWindowExW(
 		WS_EX_TOPMOST,
@@ -115,6 +123,7 @@ int main(void) {
 	ReleaseMutex(mutex);
 	DeleteObject(background_brush);
 	DeleteObject(background_brush2);
+	DeleteObject(foreground_brush);
 	return 0;
 }
 
@@ -132,40 +141,102 @@ static LRESULT CALLBACK smenuProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lP
 			searchBoxRect.top += MAIN_WINDOW_PADDING;
 			searchBoxRect.left += MAIN_WINDOW_PADDING;
 			searchBoxRect.right -= MAIN_WINDOW_PADDING;
-			searchBoxRect.bottom = searchBoxRect.top + (DEFAULT_FONT_SIZE + (SEARCH_BOX_WINDOW_PADDING << 1));
+			searchBoxRect.bottom = searchBoxRect.top + (DEFAULT_SEARCH_FONT_SIZE + (SEARCH_BOX_WINDOW_PADDING << 1));
 			FillRect(dc, &searchBoxRect, background_brush2);
 
 			searchBoxRect.top += SEARCH_BOX_WINDOW_PADDING;
 			searchBoxRect.left += SEARCH_BOX_WINDOW_PADDING;
 			SetTextColor(dc, DEFAULT_FOREGROUND_COLOR);
 			SetBkMode(dc, TRANSPARENT);
-			SelectObject(dc, ui_font);
+			SelectObject(dc, search_font);
 			DrawTextW(dc, input, input_len, &searchBoxRect, DT_LEFT | DT_TOP);
+
+			SelectObject(dc, options_font);
+			RECT optionRect = searchBoxRect;
+			optionRect.top = optionRect.bottom + (SEARCH_BOX_MARGIN + SEARCH_BOX_WINDOW_PADDING); 
+			optionRect.bottom = optionRect.top + DEFAULT_OPTIONS_FONT_SIZE + SEARCH_BOX_WINDOW_PADDING; 
+			int option = 0;
+			while (optionRect.bottom <= ps.rcPaint.bottom - MAIN_WINDOW_PADDING && option < matching_count) {
+				if (option == selected) {
+					SetTextColor(dc, DEFAULT_BACKGROUND_COLOR);
+					RECT boxRect = optionRect;
+					boxRect.top -= SEARCH_BOX_WINDOW_PADDING;
+					boxRect.left -= SEARCH_BOX_WINDOW_PADDING;
+					FillRect(dc, &boxRect, foreground_brush);
+					DrawTextW(dc, widestr_alloc + matching_strs[option], -1, &optionRect, DT_LEFT | DT_TOP);
+					SetTextColor(dc, DEFAULT_FOREGROUND_COLOR);
+				} else {
+					DrawTextW(dc, widestr_alloc + matching_strs[option], -1, &optionRect, DT_LEFT | DT_TOP);
+				}
+				optionRect.top += (DEFAULT_OPTIONS_FONT_SIZE + (SEARCH_BOX_WINDOW_PADDING << 1));
+				optionRect.bottom += (DEFAULT_OPTIONS_FONT_SIZE + (SEARCH_BOX_WINDOW_PADDING << 1));
+				option += 1;
+			}
+
 			EndPaint(hWnd, &ps);
 			return 0;
 		}
 		case WM_CHAR: {
+			unsigned char check_matches = 0;
 			switch (wParam) {
 				case 0x1B: {
 					PostQuitMessage(0);
-					return 0;
+					break;
 				} 
 				case '\b': {
 					if (input_len > 0) {
 						input_len -= 1;
+						input[input_len] = '\0';
 						InvalidateRect(hWnd, NULL, TRUE);
 					}
-					return 0;
+					memcpy(matching_strs, widestrs, widestrs_count * sizeof(widestrs[0]));
+					matching_count = widestrs_count;
+					check_matches = input_len != 0;
+					break;
+				}
+				case '\r': {
+					if (run_as_app_launcher) {
+						u16 path[MAX_PATH];
+						int folder = matching_strs[selected] < last_user_application
+							? CSIDL_PROGRAMS
+							: CSIDL_COMMON_PROGRAMS;
+						SHGetFolderPathW(NULL, folder, NULL, SHGFP_TYPE_CURRENT, path);
+						wcscat(path, L"\\");
+						wcscat(path, widestr_alloc + matching_strs[selected]);
+						wprintf(L"%s\n", path);
+						HINSTANCE res = ShellExecuteW(NULL, L"open", path, NULL, NULL, SW_SHOWNORMAL);
+						if ((int)res <= 32) {
+							wprintf(L"Error: %d, %d.\n", res, ERROR_FILE_NOT_FOUND);
+						}
+					} else {
+						fputws(widestr_alloc + matching_strs[selected], stdout);
+					}
+					PostQuitMessage(0);
+					break;
 				}
 				default: {
-					if (isNotInvalidAscii(wParam) && input_len < INPUT_BUF_SIZE) {
+					if (isNotInvalidAscii(wParam) && input_len < (INPUT_BUF_SIZE-1)) {
 						input[input_len] = wParam;
 						input_len += 1;
+						input[input_len] = '\0';
 						InvalidateRect(hWnd, NULL, TRUE);
+						check_matches = 1;
 					}
-					return 0;
+					break;
 				}
 			}
+			if (check_matches) {
+				u16 previous_matches_count = matching_count;
+				u16 new_matches = 0;
+				for (size_t i = 0; i < previous_matches_count; ++i) {
+					if (StrStrIW(widestr_alloc + matching_strs[i], input) != NULL) {
+						matching_strs[new_matches] = matching_strs[i];
+						new_matches += 1;
+					}
+				}
+				matching_count = new_matches;
+			}
+			return 0;
 		}
 		case WM_KILLFOCUS: {
 			PostQuitMessage(0);
@@ -201,11 +272,12 @@ static void parseStdin(void) {
 	}
 }
 
-static void parseFolder(u16 *path) {
+static void parseFolder(u16 *path, u8 apps_root_len) {
 	WIN32_FIND_DATAW find_data;
 	size_t len = wcslen(path);
 	HANDLE find_handle = FindFirstFileW(path, &find_data);
 	path[len-1] = '\0';
+	wprintf(L"%s : %s\n", path, path + apps_root_len);
 	if (find_handle == INVALID_HANDLE_VALUE) {
 		MessageBoxA(NULL, "smenu wasn't able to open an application folder.", "smenu error", MB_ICONERROR | MB_OK);
 		ExitProcess(1);
@@ -215,12 +287,12 @@ static void parseFolder(u16 *path) {
 			unsigned char isDesktopIni = wcscmp(find_data.cFileName, L"desktop.ini") == 0;
 			if (isDesktopIni) continue;
 			widestrs[widestrs_count] = widestr_alloc_pos;
-			for (size_t i = 0; find_data.cFileName[i] != '\0'; i += 1) {
-				widestr_alloc[widestr_alloc_pos] = find_data.cFileName[i];
-				widestr_alloc_pos += 1;
-			}
-			widestr_alloc[widestr_alloc_pos] = '\0';
-			widestr_alloc_pos += 1;
+			int written = swprintf(
+				widestr_alloc + widestr_alloc_pos,
+				WIDESTRING_ALLOC_BUF_SIZE - widestr_alloc_pos,
+				L"%s%s", path + apps_root_len, find_data.cFileName
+			);
+			widestr_alloc_pos += written + 1;
 			widestrs_count += 1;
 		} else {
 			unsigned char isCurrDir = wcscmp(find_data.cFileName, L".") == 0;
@@ -228,7 +300,8 @@ static void parseFolder(u16 *path) {
 			if (isCurrDir || isAboveDir) continue;
 			u16 new_dir[MAX_PATH];
 			swprintf(new_dir, MAX_PATH-1, L"%s%s\\*", path, find_data.cFileName);
-			parseFolder(new_dir);
+
+			parseFolder(new_dir, apps_root_len);
 		}
 	} while (FindNextFileW(find_handle, &find_data));
 	path[len-1] = '*';
